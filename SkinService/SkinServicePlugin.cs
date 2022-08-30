01450eb78d09485d56d77a33680d97a15c3f4038
kmyuhkyuk <2451614940@qq.com>
2022-08-31 02:50:29 +0800

    Update

--- SkinService/SkinServicePlugin.cs
+++ SkinService/SkinServicePlugin.cs
@@ -12,7 +12,7 @@ using SkinService.Utils;
 
 namespace SkinService
 {
-    [BepInPlugin("com.kmyuhkyuk.SkinService", "kmyuhkyuk-SkinService", "1.0.0")]
+    [BepInPlugin("com.kmyuhkyuk.SkinService", "kmyuhkyuk-SkinService", "1.1.0")]
     public class SkinServicePlugin : BaseUnityPlugin
     {
         public static ISession Session;
