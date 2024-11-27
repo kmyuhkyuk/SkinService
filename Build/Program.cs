687536d1c7f344c40cae1cd8e9a8c45919c34d02
kmyuhkyuk <2451614940@qq.com>
2024-11-27 11:22:35 +0800

    Update

--- Build/Program.cs
+++ Build/Program.cs
@@ -22,7 +22,7 @@ namespace Build
 
             const string versionName = "1.3.0";
 
-            var releaseName = $"{new DirectoryInfo(modPath).Name}-(Release_{versionName}).7z";
+            var releaseName = $"{new DirectoryInfo(modPath).Name}(Release_{versionName}).7z";
 
             try
             {
